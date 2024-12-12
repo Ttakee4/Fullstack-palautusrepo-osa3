0d48b09b8e294430706e7ba484b153c919165edc
Ttakee4 <eetumikael9@gmail.com>
2024-12-12 15:46:32 +0200

    ainakin 3.15 ja 3.16, 3.17 testiint

--- index.js
+++ index.js
@@ -51,15 +51,22 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
 	Person.findById(request.params.id).then(person => {
-		response.json(person)
-	})
+		if(person){
+			response.json(person)
+		} else {
+			response.status(404).end()
+		}
+	}).catch(error => next(error))
 })
+	
+		
 
 app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
-  
-	response.status(204).end()
+	Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -78,7 +85,7 @@ app.post('/api/persons', (request, response) => {
 
 	if (persons.find(person => person.name === body.name)){
 		return response.status(400).json({ 
-			error: 'name must be unique' 
+			error: 'name must be unique'  //Tarvitseeko nämä olla errorhandlerissä? Kun eivät ole varsinaisia erroreita.
 		  })
 	}
 
@@ -92,6 +99,39 @@ app.post('/api/persons', (request, response) => {
 	})
 
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+	const body = request.body
+
+	const person = {
+		name: body.name,
+		number: body.number,
+	}
+
+	Person.findByIdAndUpdate(request.params.id, person, { new: true})
+	.then(updatePerson => {
+		response.json(updatePerson)
+	})
+	.catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+  
+	if (error.name === 'CastError') {
+	  return response.status(400).send({ error: 'malformatted id' })
+	}
+  
+	next(error)
+  }
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
